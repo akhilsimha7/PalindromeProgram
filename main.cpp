74093903b5504cf96040043d2936c61ac77491cb
Akhil Simha <akhilsimha1059@gmail.com>
2024-01-19 01:04:34 +0100

    Palindrome Program

---
+++ main.cpp
@@ -0,0 +1,24 @@
+// Code to test if the input text is palindrome or not.
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string userInput;
+    bool status = true;
+
+    cout << "Please enter the word to check if its Palindrome:";
+    cin >> userInput;
+    int length = userInput.size();
+    for (int i = 0; i < length; i++)
+    {
+        if (userInput[i] != userInput[length - i - 1])
+        {
+            status = false;
+            break;
+        }
+    }
+    cout << "Palindrome: " << status;
+}
